73eb8c3a3e511700b2d4324cbc978994854b267f
Stan Sydorenko <stan.sydorenko@gmail.com>
2022-08-13 23:09:17 +0100

    Set urlPathStrategy to remove hash from path

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -30,6 +30,7 @@ class RegisteredRoutes {
 
 GoRouter createGoRouter() {
   return GoRouter(
+    urlPathStrategy: UrlPathStrategy.path,
     routes: [
       RegisteredRoutes.home,
       RegisteredRoutes.about,
