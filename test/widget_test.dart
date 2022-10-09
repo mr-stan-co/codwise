dc4ea0660bd9aee6a894ab5dd319827fa4df767e
Stan Sydorenko <stan.sydorenko@gmail.com>
2022-10-09 20:21:07 +0100

    Update app icons

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:codwise/app/codwise_app.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(WiseDevApp());
+    await tester.pumpWidget(CodwiseApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
