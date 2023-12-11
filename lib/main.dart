04d72923e275ead41a01def8da920713e0fdefd9
Stan Sydorenko <stan.sydorenko@gmail.com>
2023-12-11 16:34:10 +0000

    Remove unused firebase dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:codwise/app/codwise_app.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
-import 'firebase_options.dart';
-
 void main() async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   usePathUrlStrategy();
-  runApp(CodwiseApp());
+  runApp(const CodwiseApp());
 }
